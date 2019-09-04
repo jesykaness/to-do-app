027fdc811d72254322a22b8a8bb55d8af10b861f
jess <jesyka.cheng@gmail.com>
2019-09-03 22:02:43 -0700

    checkpoint domscriptingevents p1 completed

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDolist');
-  addToDoForm.addEventListener('submit', () => {
+  const toDoList = document.getElementById('toDoList');
+  addToDoForm.addEventListener('submit', (event) => {
     event.preventDefault();
     //get the text
     let title = newToDoText.value;
     //create a new li
     let newLi = document.createElement('li');
+    //create a new input
+    let checkbox = document.createElement('input');
     //set the input's type of checkbox
     checkbox.type = "checkbox";
+    //delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener('click', function(event){
+      toDoList.removeChild(this.parentElement);
+    })
     //set the title
     newLi.textContent = title;
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
     //attach the li to the ul
     toDoList.appendChild(newLi);
     //empty the input
