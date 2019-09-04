700215d3fd9b1ee9e105103908f5b6a8a63cd064
jess <jesyka.cheng@gmail.com>
2019-09-04 00:35:56 -0700

    assignment domscriptingevents p2 completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   let toDos = [];
+  let id = 0;
 
   function renderTheUI(){
     const toDoList = document.getElementById('toDoList');
@@ -10,10 +11,21 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
     });
   }
 
@@ -22,8 +34,12 @@ function onReady() {
     if(!newToDoText.value){ return; }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
+
     newToDoText.value = '';
     renderTheUI();
   }
